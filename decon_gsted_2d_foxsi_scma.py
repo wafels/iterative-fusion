df6ccf26b055f9a51ac0f27db1ee07d696f1ce9e
Jack Ireland <jackireland@gmail.com>
2016-06-01 21:14:12 +0100

    better generalization of the routine

--- decon_gsted_2d_foxsi_scma.py
+++ decon_gsted_2d_foxsi_scma.py
@@ -12,6 +12,7 @@ from scipy.signal import fftconvolve
 import scipy.stats as st
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.cm as cm
 from scipy.io import readsav
 plt.ion()
 
@@ -35,40 +36,20 @@ def test_psf(nx, sigma):
     return gkern(nx, sigma)
 
 
-def foxsi_convolve(detector, image, function='test'):
+def get_psf(detector, function='test'):
     """
-    Convolves an input image with the PSF from a specified detector
-    :param this_detector: the FOXSI detector we are interested in
-    :param image: input image
-    :return: the convolution of the input image with the PSF from the
-    specified detector
+    Defines a PSF
     """
     if function == 'test':
         sigma = test_gaussian_width(detector)
         psf = test_psf(nx, sigma)
     else:
-        pass
+        psf = None
 
-    answer = fftconvolve(image, psf, 'same')
-    return answer
-
-
-def transmogrify(img, sigma):
-    """
-    Make a noisy, blurred version of the input image
-    :param img:
-    :param sigma:
-    :return:
-    """
-    nx = img.shape[0]
-    psf = test_psf(nx, sigma)
-    img2 = fftconvolve(img, psf, 'same')
-    #img2 = gaussian_filter(sigma * img, sigma=sigma)
-    return np.random.poisson(img2)
-    return img2
+    return psf
 
 
-def show_image_set(images, title):
+def show_image_set(images, title=""):
     """
     Make a matplotlib plot of the image set
     :param images:
@@ -76,13 +57,65 @@ def show_image_set(images, title):
     """
     n_images = images.shape[0]
     ncols = int(np.ceil(np.sqrt(1.0 * n_images)))
-    nrows = int(n_images/ncols)
+    nrows = int(np.ceil(n_images/(1.0*ncols)))
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
     for nrow in range(0, nrows):
         for ncol in range(0, ncols):
             n = ncol + nrow*ncols
-            axes[nrow, ncol].imshow(images[n, :, :])
+            if n <= n_images - 1:
+                axes[nrow, ncol].imshow(images[n, :, :], cmap=cm.gray)
+                axes[nrow, ncol].set_title('{:s} {:n}'.format(title, n+1))
     plt.show()
+
+
+class DetectedImage:
+    def __init__(self, detected, psf):
+        self.detected = detected
+        self.psf = psf
+
+
+def ingaramo_deconvolution(measurement, psfs, n_iterations):
+
+    n_images = measurement.shape[0]
+    ny = measurement.shape[1]
+    nx = measurement.shape[2]
+
+    # Blurred estimate of the source image
+    blurred_estimate = np.zeros_like(measurement)
+
+    # The initial estimate
+    estimate = np.ones((ny, nx))
+
+    # Keep a history of the evolution of the estimate of the source
+    source_estimate_history = np.zeros((n_iterations + 1, ny, nx))
+
+    # Begin the deconvolution
+    source_estimate_history[0, :, :] = estimate
+    for i in range(n_iterations):
+        print("Iteration", i)
+
+        for j in range(n_images):
+            # Take the estimated true image and apply the PSF
+            psf = psfs[j, :, :]
+            blurred_estimate[j, :, :] = fftconvolve(estimate, psf, 'same') # foxsi_convolve(this_detector, estimate, function=analysis_type) # np.convolve(psf[t], estimate)  # gaussian_filter(signal_range[t] * estimate, sigma=sigma_range[t])
+
+        # Compute correction factor
+        correction_factor = np.divide(measurement, blurred_estimate)
+
+        print(" Blurring correction ratio...")
+        for j in range(n_images):
+            # Update the correction factor
+            psf = psfs[j, :, :]
+            correction_factor[j, :, :] = fftconvolve(correction_factor[j, :, :], psf, 'same')  # foxsi_convolve(this_detector, correction_factor[j, :, :], function=analysis_type)  # gaussian_filter(signal_range[t] * correction_factor[t, :, :], sigma=sigma_range[t])
+
+        # Update the estimate
+        estimate = np.multiply(estimate, correction_factor.mean(axis=0))
+
+        # Save the evolution of the estimate
+        print(source_estimate_history[i+1, :, :].min(), source_estimate_history[i, :, :].max())
+        source_estimate_history[i+1, :, :] = estimate
+
+    return source_estimate_history
 
 
 analysis_type = 'test'
@@ -116,7 +149,10 @@ else:
             observed_images = np.zeros((n_observed_images, ny, nx))
 
         gaussian_width = test_gaussian_width(i)
-        observed_images[i, :, :] = transmogrify(test_image, gaussian_width)
+        psf = test_psf(nx, gaussian_width)
+        simulated_image = np.random.poisson(fftconvolve(test_image, psf, 'same'))
+        observed_images[i, :, :] = simulated_image
+        observed_image = DetectedImage(simulated_image, psf)
 
 # Which images do we want to deconvolve
 these_images = [0, 1, 2, 3, 4, 5, 6]
@@ -128,42 +164,41 @@ n_iterations = 10
 # The measurements we are interested in
 measurement = observed_images[these_images, :, :]
 
-# Blurred estimate of the source image
-blurred_estimate = np.zeros_like(measurement)
+# PSFs
+psfs = np.zeros((len(these_images), ny, nx))
+for i, detector in enumerate(these_images):
+    psfs[i, :, :] = get_psf(detector, function=analysis_type)
 
-# Keep a history of the evolution of the estimate of the source
-source_estimate_history = np.zeros((n_iterations+1, ny, nx))
+source_estimate_history = ingaramo_deconvolution(measurement, psfs, n_iterations)
 
-# The initial estimate
-estimate = np.ones((ny, nx))
+show_image_set(measurement, title='Measurement #')
+show_image_set(source_estimate_history[1:, :, :], title='Ingaramo i =')
 
-print("Ingaramo deconvolution of images of noisy source...")
-source_estimate_history[0, :, :] = estimate
-for i in range(n_iterations):
-    print("Iteration", i)
+# Sum all the images and their PSFs and deconvolve them
+summed_measurement = np.zeros((1, ny, nx))
+summed_measurement[0, :, :] = np.sum(measurement, axis=0).reshape(1, ny, nx)
+summed_psf = np.zeros_like(summed_measurement)
+summed_psf[0, :, :] = np.sum(psfs, axis=0).reshape(1, ny, nx)
 
-    for j, this_detector in enumerate(these_images):
-        #
-        # Take the estimated true image and apply the PSF
-        #
-        blurred_estimate[j, :, :] = foxsi_convolve(this_detector, estimate, function=analysis_type) # np.convolve(psf[t], estimate)  # gaussian_filter(signal_range[t] * estimate, sigma=sigma_range[t])
-    #
-    # Compute correction factor
-    #
-    correction_factor = np.divide(measurement, blurred_estimate)
+source_estimate_history = ingaramo_deconvolution(summed_measurement, summed_psf, n_iterations)
+show_image_set(source_estimate_history[1:, :, :], title='Summed i =')
 
-    print(" Blurring correction ratio...")
-    for j, this_detector in enumerate(these_images):
-        #
-        # Update the correction factor
-        #
-        correction_factor[j, :, :] = foxsi_convolve(this_detector, correction_factor[j, :, :], function=analysis_type)  # gaussian_filter(signal_range[t] * correction_factor[t, :, :], sigma=sigma_range[t])
+# Do each image separately and add up the final results
+final_images = np.zeros((n_images, ny, nx))
+for i, detector in enumerate(these_images):
+    this_measurement = np.zeros((1, ny, nx)).reshape(1, ny, nx)
+    this_measurement[0, :, :] = measurement[detector, :, :].reshape(1, ny, nx)
+    this_psf = np.zeros_like(this_measurement).reshape(1, ny, nx)
+    this_psf[0, :, :] = psfs[detector, :, :].reshape(1, ny, nx)
 
-    estimate = np.multiply(estimate, correction_factor.mean(axis=0))
+    source_estimate_history = ingaramo_deconvolution(this_measurement,
+                                                     this_psf,
+                                                     n_iterations)
 
-    # Save the evolution of the estimate
-    print(source_estimate_history[i+1, :, :].min(), source_estimate_history[i, :, :].max())
-    source_estimate_history[i+1, :, :] = estimate
+    final_images[detector, :, :] = source_estimate_history[-1, :, :]
 
 
-# Deconvolve each image separately for comparison
+final_image = np.sum(final_images, axis=0)
+plt.figure()
+plt.imshow(final_image, cmap=cm.gray)
+plt.show()
